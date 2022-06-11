5881bd4ea51e620815d611831ad8515b5e3addba
Khoa Luong <luongkhoa95@gmail.com>
2022-06-11 16:09:19 -0700

    Initial commit

---
+++ Source/GalaxySuperExpress/GalaxySuperExpressGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GalaxySuperExpressGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GALAXYSUPEREXPRESS_API AGalaxySuperExpressGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GalaxySuperExpress/Public/Yu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperFlipbookActor.h"
+#include "Yu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GALAXYSUPEREXPRESS_API AYu : public APaperFlipbookActor
+{
+	GENERATED_BODY()
+	
+};
